2a0c1a4b88c84dd1fc995652dd9556d07d1ec24b
r1k1 <yu.izanagi.persona4@pm.me>
2026-08-21 20:47:46 +1200

    renaming

--- Assets/Scripts/ItemDrops.cs
+++ Assets/Scripts/ItemDrops.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class ItemSpawner : MonoBehaviour
+public class ItemDrops : MonoBehaviour
 {
     [SerializeField] private GameObject[] itemPrefabs;
     [SerializeField] private Transform player;
