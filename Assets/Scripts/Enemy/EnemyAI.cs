cbb79ed859f12d95ccd2b3cfeeba8724b23bf0ae
aslesmv <aslesmv@gmail.com>
2026-08-22 13:58:47 +1200

    1:58pm
    - Got rid of the bacteria idle anim stuff as it was not needed

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -6,17 +6,8 @@ public class EnemyAI : MonoBehaviour
 
     private Transform player;
 
-    private Animator anim;
-    private Rigidbody2D rb;
-    [SerializeField] private string idleStateName = "Bacteria_Idle";
-
     void Start()
     {
-        anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-
-        anim.Play("Bacteria_Idle");
-
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
 
         if (playerObject != null)
@@ -31,18 +22,5 @@ public class EnemyAI : MonoBehaviour
             return;
 
         transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-    }
-
-    private void LateUpdate()
-    {
-        // Gets the current animation to play from the base layer
-        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-
-        // Forcing the AI to drag the Idle back in
-        if (!stateInfo.IsName(idleStateName))
-        {
-            // Forcing the animator to go back to the idle loop instantly
-            anim.Play(idleStateName, 0, stateInfo.normalizedTime);
-        }
     }
 }
